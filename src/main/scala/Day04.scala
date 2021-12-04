a6d1874c70e0783bfe99fbcbfb9733d2e85386ea
Doug Gallagher <8922663+douggal@users.noreply.github.com>
2021-12-04 16:21:22 -0500

    Day04 pt 1 create bingo boards 2

--- src/main/scala/Day04.scala
+++ src/main/scala/Day04.scala
@@ -1,4 +1,6 @@
 import scala.collection.mutable.ListBuffer
+import scala.util.Using
+import scala.util.{Try, Success, Failure}
 
 object Day04 extends App {
 
@@ -9,14 +11,35 @@ object Day04 extends App {
 
   //val filename = "Day04Input.txt"
   val filename = "testInput.txt"
-  val bufferedSource = scala.io.Source.fromFile(filename)
-  val input = bufferedSource
-    .getLines
-    .map(line => line.strip)
-    .toSeq
 
-  bufferedSource.close
+  // Better read file w/Using object from Alexander, Alvin.
+  // Scala Cookbook: Recipes for Object-Oriented and Functional Programming.
+  // Second Edition. Beijing Boston: O’Reilly, 2021.
+  def readFileAsSeq(filename: String): Try[Vector[String]] =
+    Using(io.Source.fromFile(filename)) { bufferedSource =>
+      val ucLines = for
+        line <- bufferedSource.getLines
+      // 'line' is a String. can work with each Char here,
+      // if desired, like this:
+      // char <- line
+      yield
+        // work with each 'line' as a String here
+        line.strip
+      ucLines.toVector
+    }
 
+  val input: Vector[String] = readFileAsSeq(filename) match
+    case Success(i) => {
+      println(s"Success, file read")
+      i
+    }
+    case Failure(s) => {
+      println(s"Failed to read input file, message is: $s")
+      Vector[String]()
+    }
+
+  // a bingo board, two lists one for the board's numbers
+  // and the other to track which spots are marked
   class bingoBoard(val board: Vector[Int]) {
     // create a list 0's/1's to hold marked squares
     private val _markerList = ListBuffer[Int]()
@@ -24,7 +47,7 @@ object Day04 extends App {
 
     def bingo(): Boolean = {
       // TODO: write a check for bingo
-      ???
+      true
     }
   }
 
@@ -32,18 +55,18 @@ object Day04 extends App {
   val numberCaller = input.head.split(',').toList.map(_.toInt)
   //println(numberCaller)
 
-  // set up game board(s)
+  // set up game board(s), keep them in a list
   val bingoBoards = ListBuffer[bingoBoard]()
   var l = ""
-  for (line <- input.tail)
+  for (line <- input.tail) {
     if (line != "")
       l += " " + line
-    else
-      if (line == "" && l != "")
-        bingoBoards += new bingoBoard(l.split("\\s+",-1).toVector.drop(1).map(_.toInt))
-        l = ""
-     // pick up last board if there is one
-    if (l != "")  bingoBoards += new bingoBoard(l.split("\\s+",-1).toVector.drop(1).map(_.toInt))
+    else if (line == "" && l != "")
+      bingoBoards += new bingoBoard(l.split("\\s+", -1).toVector.drop(1).map(_.toInt))
+      l = ""
+  }
+  // pick up last board if there is one
+  if (l != "")  bingoBoards += new bingoBoard(l.split("\\s+",-1).toVector.drop(1).map(_.toInt))
 
   val i = 0
 
