37353fbc96847c11d6a66fd4bb557bc6960fa0b1
Doug Gallagher <8922663+douggal@users.noreply.github.com>
2021-12-04 16:52:39 -0500

    Day04 pt 1 change default indent size

--- src/main/scala/Day04.scala
+++ src/main/scala/Day04.scala
@@ -4,79 +4,82 @@ import scala.util.{Try, Success, Failure}
 
 object Day04 extends App {
 
-  // created 12/04/2021
-  // https://adventofcode.com/2021/day/4
+    // created 12/04/2021
+    // https://adventofcode.com/2021/day/4
 
-  println(s"--- Day 4: Giant Squid ---")
+    println(s"--- Day 4: Giant Squid ---")
 
-  //val filename = "Day04Input.txt"
-  val filename = "testInput.txt"
+    //val filename = "Day04Input.txt"
+    val filename = "testInput.txt"
 
-  // Better read file w/Using object from Alexander, Alvin.
-  // Scala Cookbook: Recipes for Object-Oriented and Functional Programming.
-  // Second Edition. Beijing Boston: O’Reilly, 2021.
-  def readFileAsSeq(filename: String): Try[Vector[String]] =
-    Using(io.Source.fromFile(filename)) { bufferedSource =>
-      val ucLines = for
-        line <- bufferedSource.getLines
-      // 'line' is a String. can work with each Char here,
-      // if desired, like this:
-      // char <- line
-      yield
-        // work with each 'line' as a String here
-        line.strip
-      ucLines.toVector
+    // Try out a better read file w/Using object from Alexander, Alvin.
+    // Scala Cookbook: Recipes for Object-Oriented and Functional Programming.
+    // Second Edition. Beijing Boston: O’Reilly, 2021.
+    def readFileAsSeq(filename: String): Try[Vector[String]] =
+        Using(io.Source.fromFile(filename)) { bufferedSource =>
+            val ucLines = for
+                line <- bufferedSource.getLines
+            // 'line' is a String. can work with each Char here,
+            // if desired, like this:
+            // char <- line
+            yield
+                // work with each 'line' as a String here
+                line.strip
+            ucLines.toVector
+        }
+
+    // read the input file, check for success or failure
+    val input: Vector[String] = readFileAsSeq(filename) match
+        case Success(i) =>
+            println(s"Success, file read")
+            i
+        case Failure(s) =>
+            println(s"Failed to read input file, message is: $s")
+            Vector[String]()
+
+    // model the bingo board as two lists one for the board's numbers
+    // and the other to track which spots are marked
+    class bingoBoard(val board: Vector[Int]) {
+        private val _size = 5
+        private val _bingo = false
+        // create a list 0's/1's to hold marked squares
+        private val _markerList = ListBuffer[Int]()
+        for (i <- board.indices) _markerList += 0
+
+        def bingo(): Boolean = {
+            // TODO: write a check for bingo
+            false
+        }
     }
 
-  // read the input file, check for success or failure
-  val input: Vector[String] = readFileAsSeq(filename) match
-    case Success(i) => {
-      println(s"Success, file read")
-      i
+    // first item is to set up number caller from 1st line of input
+    val numberCaller = input.head.split(',').toList.map(_.toInt)
+    //println(numberCaller)
+
+    // next set up game board(s) from the remaining file input
+    // keep them in a list
+    // TODO: seems to work ok, but can this code be improved?
+    val bingoBoards = ListBuffer[bingoBoard]()
+    var l = ""
+    for (line <- input.tail) {
+        if (line != "")
+            l += " " + line
+        else if (line == "" && l != "")
+            //those leading spaces are a tripping hazard :)
+            // https://stackoverflow.com/questions/7899525/how-to-split-a-string-by-space/7899558
+            bingoBoards += new bingoBoard(l.trim.split("\\s+", -1).toVector.map(_.toInt))
+            l = ""
     }
-    case Failure(s) => {
-      println(s"Failed to read input file, message is: $s")
-      Vector[String]()
-    }
+    // pick up last board if there is one
+    if (l != "") bingoBoards += new bingoBoard(l.trim.split("\\s+", -1).toVector.map(_.toInt))
 
-  // model the bingo board as two lists one for the board's numbers
-  // and the other to track which spots are marked
-  class bingoBoard(val board: Vector[Int]) {
-    // create a list 0's/1's to hold marked squares
-    private val _markerList = ListBuffer[Int]()
-    for (i <- 0 until board.length) _markerList += 0
-
-    def bingo(): Boolean = {
-      // TODO: write a check for bingo
-      true
-    }
-  }
-
-  // first item is to set up number caller from 1st line of input
-  val numberCaller = input.head.split(',').toList.map(_.toInt)
-  //println(numberCaller)
-
-  // next set up game board(s) from the remaining file input
-  // keep them in a list
-  // TODO: seems to work ok, but can this code be improved?
-  val bingoBoards = ListBuffer[bingoBoard]()
-  var l = ""
-  for (line <- input.tail) {
-    if (line != "")
-      l += " " + line
-    else if (line == "" && l != "")
-      bingoBoards += new bingoBoard(l.split("\\s+", -1).toVector.drop(1).map(_.toInt))
-      l = ""
-  }
-  // pick up last board if there is one
-  if (l != "")  bingoBoards += new bingoBoard(l.split("\\s+",-1).toVector.drop(1).map(_.toInt))
-
-  // play BINGO!
+    // play BINGO!
+    println("Play BINGO! with the Giant Squid")
 
 
-  println(s"Day 4 Part 1 answer TBD")
-  println()
+    println(s"Day 4 Part 1 answer TBD")
+    println()
 
-  println(s"Day 4 Part 2 answer: TBD")
+    println(s"Day 4 Part 2 answer: TBD")
 
 }
