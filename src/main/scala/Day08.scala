6b18b7decf1d913f7cf8f5eda8da2d7e2d43e23b
Doug Gallagher <8922663+douggal@users.noreply.github.com>
2021-12-08 16:28:16 -0500

    Day08 part 2 init and refactor part 1

--- src/main/scala/Day08.scala
+++ src/main/scala/Day08.scala
@@ -51,18 +51,34 @@ object Day08 extends App {
 
     val digitMap = scala.collection.immutable.HashMap[Int,String](
         0 -> "abcefg", 1 -> "cf", 2 -> "acdeg", 3 -> "acdfg", 4->"bcdf",
-        5-> "abdfg", 6->"abdefg",7->"acf",8->"abcdef",9->"abcdfg")
+        5-> "abdfg", 6->"abdefg",7->"acf",8->"abcdefg",9->"abcdfg")
 
-    // In the output values, how many times do digits 1, 4, 7, or 8 appear?
-    // lod = length of each output digit string
+    // length of each digit's string represented as a 7 segment display
+    val lds = for (d <- digitMap.keys.toVector.sorted) yield digitMap(d).length
+
+    // the digits with a unique char count of its 7 segment display
+    val us = Set(1,4,7,8)
+
+    // set of string lengths of the unique digits expressed as 7 segment display
+    val lsu = lds.zipWithIndex.filter(d => us.contains(d._2)).map(_._1).toSet
+
+    // lod = length of each digit string in the data input's output section (4 digits per line)
+    // all of them in the whole file top to bottom
     val lod = input.flatMap(l => l.fdo.map(s => s.length))
     //println(lod)
 
-    val answer1 = lod.count(d => d == 2 || d == 4 || d == 3 || d == 7)
+    // answer is count of items in lod which are in set of unique digits
+    val answer1 = lod.count(d => lsu.contains(d))
 
     val duration1 = (System.nanoTime - t1) / 1e9d
     println(s"Done: Part 1 run time (by the clock): $duration1 sec")
 
     println(s"Day 8 Part 1 the number of times do digits 1, 4, 7, or 8 appear is: $answer1")
+
+
+
+
+    println(s"Day 8 Part 2 the sum of the display numbers is: ")
+
     println(s"End at ${java.time.ZonedDateTime.now()}")
 }
