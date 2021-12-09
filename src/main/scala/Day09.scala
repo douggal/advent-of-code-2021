203bc211e9e1001acd3fb6e955584e874bb8d0d2
Doug Gallagher <8922663+douggal@users.noreply.github.com>
2021-12-09 04:43:41 -0500

    Day09 part 1 read test data

--- src/main/scala/Day09.scala
+++ src/main/scala/Day09.scala
@@ -9,23 +9,23 @@ object Day09 extends App {
     //val filename = "Day09Input.txt"
     val filename = "testInput.txt"
 
-    case class inputLine(sp: Vector[String], fdo: Vector[String])
+    //Each number corresponds to the height of a particular location,
+    // where 9 is the highest and 0 is the lowest a location can be
+    case class inputLine(heightmap: Vector[Int])
 
     val readInputData = () => {
         val source = io.Source.fromFile(filename)
         for {
             line <- source.getLines().toVector
-            cols = line.split("[| ]+").map(_.trim) // split on either space or | ???
+            cols = line.strip().split("").toVector.map(_.toInt)
         } yield
-            inputLine(Vector[String](cols(0), cols(1), cols(2), cols(3), cols(4), cols(5),
-                cols(6), cols(7), cols(8), cols(9)),
-                Vector[String](cols(10), cols(11), cols(12), cols(13)))
+            inputLine(cols)
     }
 
     val input = readInputData()
-    //    for (line <- input) {
-    //        println(line)
-    //    }
+    for (line <- input) {
+        println(line)
+    }
 
     println("------------------------------------")
     println("Data Quality Control:")
@@ -33,13 +33,18 @@ object Day09 extends App {
     println(s"Input file name: $filename")
     println(s"Each line is a: ${input.getClass}")
     println(s"Number lines: ${input.length}")
-    println(s"Number items per line: ${input.head.sp.length}, ${input.head.fdo.length}")
+    println(s"Number items per line: ${input.head.heightmap.count(_ => true)}")
     println(s"Input first line: ${input.head}")
     println(s"Input last line: ${input.tail.last}")
     println("------------------------------------")
 
 
     // Part 1
+
+    //Your first goal is to find the low points - the locations that are lower than
+    // any of its adjacent locations.
+
+    // The risk level of a low point is 1 plus its height
 
     println(s"Day 9 Part 1 answer: TBD")
 
