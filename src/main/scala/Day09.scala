bd766c93656abe88e318df2e1a6e647b987ef0f9
Doug Gallagher <8922663+douggal@users.noreply.github.com>
2021-12-11 12:05:59 -0500

    Day09 part 2 no go, close but no cigar

--- src/main/scala/Day09.scala
+++ src/main/scala/Day09.scala
@@ -94,43 +94,63 @@ object Day09 extends App {
     // given a basin, walk the heightmap until can't go any further.  return is the size of the basin
     def walkUp(b: Point, v:mutable.Set[Point]):mutable.Set[Point] = {
         if (input(b.row).heightmap(b.col) == 9) v
-        else if (b.row == 0) v += b
-        else walkUp(Point(b.row-1,b.col),  v += b)
+        else if (b.row-1 < 0) v += b
+        else {
+            //println(s"walkUP b= ${b.row},${b.col},  Next = ${b.row+1},${b.col}")
+            walkUp(Point(b.row-1,b.col),  v += b)
+        }
     }
     def walkDown(b: Point, v: mutable.Set[Point]):mutable.Set[Point] = {
         if (input(b.row).heightmap(b.col) == 9) v
-        else if (b.row == input.length-1) v += b
-        else walkDown(Point(b.row+1,b.col),  v += b)
+        else if (b.row+1 > input.length-1) v += b
+        else {
+            //println(s"walkDown b= ${b.row},${b.col},  Next = ${b.row+1},${b.col}")
+            walkDown(Point(b.row+1,b.col),  v += b)
+        }
     }
     def walkRight(b: Point, v:mutable.Set[Point]):mutable.Set[Point] = {
-        if (input(b.row).heightmap(b.col) == 9) v
-        else {
+        if (input(b.row).heightmap(b.col) != 9) {
             v.addAll(walkUp(b,v))
+            println(s"walked right and Up: v= $v")
             v.addAll(walkDown(b,v))
+            println(s"walked right and Down: v= $v")
         }
 
-        if (b.col+1 == input(b.row).heightmap.length) v
-        else walkRight(Point(b.row, b.col + 1), v)
+        if (b.col+1 > input(b.row).heightmap.length-1) v += b
+        else {
+            println(s"walking right next col: b = $b, v= ${b.row},${b.col}")
+            walkRight(Point(b.row, b.col + 1), v)
+        }
     }
     def walkLeft(b: Point, v:mutable.Set[Point]):mutable.Set[Point] = {
-        if (input(b.row).heightmap(b.col) == 9) v
-        else {
+        if (input(b.row).heightmap(b.col) != 9) {
             v.addAll(walkUp(b,v))
+            println(s"walked left and Up: v= $v")
             v.addAll(walkDown(b,v))
+            println(s"walked left and Up: v= $v")
         }
 
-        if (b.col-1 < 0) v
+        if (b.col == 0) println(s"B equals 0 ${b.row},${b.col}")
+        if (b.col-1 < 0) v += b
         else walkLeft(Point(b.row,b.col-1),  v)
     }
 
     def walkBasin(p: Point):mutable.Set[Point] = {
         var b = mutable.Set[Point]()
-        walkRight(p, b) ++= walkLeft(p, b)
+        var c = (walkRight(p, b) ++= walkLeft(p, b)).toList
+        println(s"walked right and left: basin = $c")
+        while (!c.isEmpty) {
+            walkRight(c, b) ++= walkLeft(c, b)
+            c = c.drop(1)
+        }
+        b
     }
 
-    for (l <- lows) {
-        basins += walkBasin(Point(l(0),l(1)))
-    }
+    println(s"$lows")
+    println(s"$lowPoints")
+    //for (l <- lowPoints) {
+        basins += walkBasin(lowPoints(2))
+   // }
 
     val answer2 = basins.sortBy(b => b.size).take(3).map(_.size).product
 
