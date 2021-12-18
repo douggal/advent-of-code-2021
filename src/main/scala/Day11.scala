23904692c6d9d5a0d323a9002da8ce79cc5ac96c
Doug Gallagher <8922663+douggal@users.noreply.github.com>
2021-12-18 15:48:17 -0500

    Day 11 use spreadsheet style row, col coords

--- src/main/scala/Day11.scala
+++ src/main/scala/Day11.scala
@@ -45,14 +45,20 @@ object Day11 extends App {
 
     // List of lists to represent the sea floor grid
     // and leave the original input alone
+    // Use spreadsheet style coordinates, (r, c) where r is y-axis increasing downwards,
+    // and c is x-axis increasing to the right
     var grid = ListBuffer[ListBuffer[Int]]()
 
     // zip with index passes tuple-2 and case matches on tuple-2 passing l and i into the procedure
-    input.zipWithIndex.foreach {case (l, i) =>
+    input.zipWithIndex.foreach {case (l, r) =>
         grid += ListBuffer[Int]()
-        for (octo <- l.octos.zipWithIndex)  grid(i) += octo._1.toInt
+        for (octo <- l.octos.zipWithIndex)  grid(r) += octo._1.toInt
     }
     //printGrid
+
+    // coordinates on sea floor are in x (columns) and y (rows) origin top-left and y increasing downwards
+    // https://www.redblobgames.com/grids/parts/
+    case class GridPoint(r: Int, c:Int)
 
     def printGrid(gen: Int) : Unit = {
       println(s"Generation: $gen")
@@ -63,16 +69,59 @@ object Day11 extends App {
     }
 
     def addOne : Unit = {
-        grid.zipWithIndex.foreach { case (row, i) =>
-            for (o <- row.indices) grid(i)(o) += 1
+        grid.zipWithIndex.foreach { case (row, r) =>
+            for (c <- row.indices) grid(r)(c) += 1
         }
     }
 
+    def addOneNeighbors(ns: List[GridPoint]) : Unit = {
+        ns.foreach { p => grid(p.r)(p.c) += 1 }
+    }
+
+    def getFlashes: List[GridPoint] = {
+        val x = ListBuffer[GridPoint]()
+        grid.zipWithIndex.foreach { case (row, r) =>
+            for (c <- row.indices;
+                if grid(r)(c) == 10) x+= GridPoint(r,c)
+        }
+        x.toList
+    }
+
+    def getNeighbors(gp:GridPoint):List[GridPoint] = {
+        // clockwise from top center
+        // coordinates are spreadsheet form: (r,c) where
+        // rs go from top to bottom (rows) and cs go increase from left to right, origin top-left
+        val rs = List(0,1,1,1,0,-1,-1,-1)
+        val cs = List(-1,-1,0,1,1,1,0,-1)
+        val neighbors = for (p <- rs.zip(cs);
+                             if gp.r + p._1 >= 0 && gp.r + p._1 < grid.length;
+                             if gp.c + p._2 >= 0 && gp.c + p._2 < grid.head.length)
+                        yield GridPoint(gp.r + p._1 ,gp.c + p._2)
+        neighbors
+    }
+
+    def propigateFlashes(ps: List[Day11.GridPoint], cnt: Int):Int = {
+
+        if (ps.isEmpty) cnt
+        else {
+            val ns = getNeighbors(ps.head)
+        }
+        1
+    }
+
+    def doFlash: Int = {
+        val flashes = getFlashes
+        val cnt = propigateFlashes(flashes, 0)
+        flashes.length + cnt
+    }
+
     val steps = 1
+    var flashCount = 0
     printGrid(0)
     for (step <- 1 to steps) {
-        // add 1 to each octo
+        // add 1 to each octopus
         addOne
+        flashCount += doFlash
         printGrid(step)
     }
 
