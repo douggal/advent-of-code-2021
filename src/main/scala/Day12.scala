dad31e88cd2e3254337e45ecc6cb343e3a14b1d4
Doug Gallagher <8922663+douggal@users.noreply.github.com>
2021-12-19 18:46:12 -0500

    Day 12 Part Two init and build on Pt 1 work

--- src/main/scala/Day12.scala
+++ src/main/scala/Day12.scala
@@ -53,23 +53,29 @@ object Day12 extends App {
     don't visit small caves more than once.
     */
 
-    // build a dictionary of caves and to which caves they are connected
-    // Edit: works in both directions! Add reverse connection too.
-    val caveMap = mutable.HashMap[String,mutable.ListBuffer[String]]()
-    // "end" cave doesn't connect to anything
-    caveMap("end") = mutable.ListBuffer[String]()
-    for (l <- input) {
-        val cave = l.cave(0)
-        val to  = l.cave(1)
-        if (!caveMap.contains(cave))
-            caveMap(cave) = mutable.ListBuffer[String]()
-        caveMap(cave) += to
-        // reverse dirction is also possible - the splunking will handle not repeating small caves
-        if (!caveMap.contains(to))
-            caveMap(to) = mutable.ListBuffer[String]()
-        caveMap(to) += cave
+    def makeCaveMap(): mutable.HashMap[String,mutable.ListBuffer[String]] = {
+        val caveMap = mutable.HashMap[String, mutable.ListBuffer[String]]()
+        // build a dictionary of caves and to which caves they are connected
+        // Edit: works in both directions! Add reverse connection too.
+
+        // "end" cave doesn't connect to anything
+        caveMap("end") = mutable.ListBuffer[String]()
+
+        for (l <- input) {
+            val cave = l.cave(0)
+            val to = l.cave(1)
+            if (!caveMap.contains(cave))
+                caveMap(cave) = mutable.ListBuffer[String]()
+            caveMap(cave) += to
+            // reverse dirction is also possible - the splunking will handle not repeating small caves
+            if (!caveMap.contains(to))
+                caveMap(to) = mutable.ListBuffer[String]()
+            caveMap(to) += cave
+        }
+        caveMap
     }
 
+    val caveMap = makeCaveMap()
     caveMap.foreach(c => println(s"$c"))
 
     val visitedCaves = mutable.HashMap[String,Int]()
@@ -107,8 +113,38 @@ object Day12 extends App {
 
 
     // Part Two
+    val caveMapPt2 = makeCaveMap()
+    val visitedCavesPt2 = mutable.HashMap[String,Int]()
+    val passagesPt2 = mutable.ListBuffer[String]()
 
-    println(s"Day 12 Part 2 TBD")
+    def splunkPt2(c: String, p: List[String]): Unit = {
+
+        if (visitedCavesPt2.contains(c))
+            visitedCavesPt2(c) += 1
+        else
+            visitedCavesPt2 += c -> 1
+
+        if (c == "end")
+            passagesPt2.addOne(p.toString())  // the end
+            ()
+        else if (!caveMapPt2.contains(c) || caveMapPt2(c) == Nil) () // dead end
+        else // keep going
+            for (caveNext <- caveMapPt2(c))
+                if (!caveNext.contains(c))  // can't go from and to itself
+                    if (caveNext == caveNext.capitalize)  // can go to big cave as many times as needed
+                        splunkPt2(caveNext, p ::: List(caveNext))
+                    else if (!p.contains(caveNext))  // small cave visit only once
+                        splunkPt2(caveNext, p ::: List(caveNext))
+                    else ()  // dead ended
+    }
+
+    splunkPt2("start",List("start"))
+
+    passagesPt2.foreach(println(_))
+
+    val answerPt2 = passagesPt2.length
+
+    println(s"Day 12 Part 2 allowing one small cave to be revisited there are now ${answerPt2} through this cave system.")
 
     println(s"End at ${java.time.ZonedDateTime.now()}")
 
